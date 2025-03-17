609e792a2617e6b69179a8fea33e2fae84a8e5e0
Simon Chatterley <simon.chatterley@fdmgroup.com>
2025-03-17 09:16:25 +0000

    updated main.py dependency injection for embedder

--- backend/main.py
+++ backend/main.py
@@ -2,11 +2,13 @@ import uvicorn
 
 from config import (OPENAI_API_KEY)
 from driver import Neo4jDriver
+from embedding import Embedding
 from api import create_api
 
 
 api_key = OPENAI_API_KEY
 driver = Neo4jDriver.get_instance().driver
+embedder = Embedding.get_instance().embedder
 app = create_api
 
 @app.on_event("shutdown")
